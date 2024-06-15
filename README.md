ff6b7d677a2133bae4e64661c416b0be2655259c
IsaiDiaz <jsoza057@gmail.com>
2024-06-14 22:07:01 -0400

    initializing wtg_employee_app

---
+++ README.md
@@ -0,0 +1,3 @@
+# wtg_employee_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/wtg_employee_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wtg_employee_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
